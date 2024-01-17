a428a9aac172b85bde5e5f055f2157fb0235aa4e
Robert Babaev <github@robertbabaev.tech>
2024-01-16 23:09:45 -0500

    switch to vite for client

--- server/resume_generator/main.py
+++ server/resume_generator/main.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
 import uvicorn
 
 app = FastAPI()
 
+app.mount("/", StaticFiles(directory="build"), name="build")
+app.mount("/data", StaticFiles(directory="data"), name="data")
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def root():
+    index_path = Path("build") / "index.html"
+
     return {"message": "Hello World"}
 
 if __name__ == "__main__":
