179e7bf3d0bfd639a6e67c0659e2b639e0b9f91e
Robert Babaev <github@robertbabaev.tech>
2024-01-17 21:07:01 -0500

    add client scaffolding

---
+++ client/src/components/FileUploadSingle.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+
+
+function FileUploadSingle({ file, onChange, onFileLoad, text }) {
+
+    return (
+        <div>
+            <input type="file" onChange={onChange} />
+
+            <div>{file && `${file.name} - ${file.type}`}</div>
+
+            <button onClick={onFileLoad}>{text}</button>
+        </div>
+    );
+}
+
+FileUploadSingle.propTypes = {
+    file: PropTypes.object,
+    onChange: PropTypes.func,
+    onFileLoad: PropTypes.func,
+    text: PropTypes.string
+};
+
+export default FileUploadSingle;
